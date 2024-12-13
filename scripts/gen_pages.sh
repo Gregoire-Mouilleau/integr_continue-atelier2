575b472c16528da652eec5672fe27ce3954117ce
Grégoire MOUILLEAU <115691858+Gregoire-Mouilleau@users.noreply.github.com>
2024-12-13 15:51:36 +0100

    Update gen_pages.sh

--- scripts/gen_pages.sh
+++ scripts/gen_pages.sh
@@ -8,6 +8,10 @@ mkdir -p $SITE_DIR
 for file in "$SOURCE_DIR"/*.md; do
     if [[ -f "$file" ]]; then
         filename=$(basename "$file" .md)
+        echo "Processing $file"
         pandoc "$file" -o "$SITE_DIR/$filename.html"
+        echo "$SITE_DIR/$filename.html generated."
+    else
+        echo "No Markdown files found in $SOURCE_DIR."
     fi
 done
