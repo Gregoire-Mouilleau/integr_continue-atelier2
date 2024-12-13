a4ac5592bea0e974ed47833a2f0e74a4a9b2a7c8
Grégoire MOUILLEAU <115691858+Gregoire-Mouilleau@users.noreply.github.com>
2024-12-13 15:36:46 +0100

    Update gen_pages.sh

--- scripts/gen_pages.sh
+++ scripts/gen_pages.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 SOURCE_DIR="./sources"
-SITE_DIR="../site"
+SITE_DIR="./site"
 
-Utilisation de Pandoc pour convertir les fichiers Markdown en HTML
 for file in "$SOURCE_DIR"/*.md; do
     if [[ -f "$file" ]]; then
-        filename=$(basename "$file" .md)  
+        filename=$(basename "$file" .md)
         pandoc "$file" -o "$SITE_DIR/$filename.html"
-        echo "$filename.html généré."
-    else
-        echo "Aucun fichier Markdown trouvé dans $SOURCE_DIR."
     fi
 done
