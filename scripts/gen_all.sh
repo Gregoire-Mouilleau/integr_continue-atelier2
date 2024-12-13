0993551421a4d9f6933281b6843bcc9599027019
Grégoire MOUILLEAU <115691858+Gregoire-Mouilleau@users.noreply.github.com>
2024-12-13 15:36:02 +0100

    Update gen_all.sh

--- scripts/gen_all.sh
+++ scripts/gen_all.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 SOURCE_DIR="./sources"
-SITE_DIR="../site"
+SITE_DIR="./site"
 
 mkdir -p $SITE_DIR
-echo "Génération des pages..."
+
 ./gen_pages.sh
-
-echo "Génération de l'index..."
 python3 gen_index.py
-
-echo "Génération de la page du bureau..."
 python3 gen_bureau.py
-
-echo "Génération terminée."
